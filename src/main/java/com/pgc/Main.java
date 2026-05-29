a80a7083f553d4bf0aca6bd9619f2ba2bdc17733
Pablo Guerra Camana <pguerracamana@gmail.com>
2026-05-29 01:23:57 -0500

    fix: inicializacion doble de la bbdd

--- src/main/java/com/pgc/Main.java
+++ src/main/java/com/pgc/Main.java
@@ -1,23 +1,29 @@
 package com.pgc;
 
+import com.pgc.db.DBConnection;
 import com.pgc.processor.MainProcessor;
 import com.pgc.service.OpenDotaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import skadistats.clarity.processor.runner.SimpleRunner;
 import skadistats.clarity.source.MappedFileSource;
 import skadistats.clarity.source.Source;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
-            System.out.println("Debes pasar un replay .dem");
+            log.error("Debes pasar un replay .dem");
             return;
         }
+
+        DBConnection dbConnection = runDbConnection();
 
         Source source = new MappedFileSource(args[0]);
         SimpleRunner runner = new SimpleRunner(source);
 
         OpenDotaService openDotaService = new OpenDotaService();
-
         openDotaService.downloadMatch(args[0]);
 
         MainProcessor processor = new MainProcessor();
@@ -26,6 +32,33 @@ public class Main {
 //                processor
 //        );
 
-        System.out.println("Replay procesado correctamente.");
+        dbConnection.close();
+
+        log.info("Replay procesado correctamente.");
+    }
+
+    private static DBConnection runDbConnection() {
+        DBConnection dbConnection = DBConnection.getInstance();
+        dbConnection.init();
+
+        dbConnection.registerStatement(
+                "insert_match",
+                """
+                INSERT INTO Match (
+                    match_id,
+                    start_date_time,
+                    end_date_time,
+                    duration_seconds,
+                    dire_team_id,
+                    radiant_team_id,
+                    replay_url,
+                    patch,
+                    first_blood_time_seconds,
+                    radiant_win
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """
+        );
+        return dbConnection;
     }
 }
